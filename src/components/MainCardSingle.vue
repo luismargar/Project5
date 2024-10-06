cba57e0fe52f30067f10b57ec6dfb9d2781d0ff3
Luis Martinez <lmartin1@stu.southtexascollege.edu>
2024-10-06 12:28:09 -0500

    card components added

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { faker } from '@faker-js/faker'
+const fullname = faker.person.fullName()
+const lastname = faker.person.lastName()
+
+
+</script>
+
+<template>
+    {{ fullname }} <br />
+
+
+
+
+</template>
